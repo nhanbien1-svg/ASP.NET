bcf8fd4593eeae84b309d0f37c7498df84e5a603
ASP.NET <nhanbien1@gmail.com>
2026-05-15 15:17:51 +0700

    update

---
+++ CMS.DATA/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace CMS.DATA.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedData { get; set; } = DateTime.Now;
+        // Khóa ngoại kết nối với Category
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+    }
+}
