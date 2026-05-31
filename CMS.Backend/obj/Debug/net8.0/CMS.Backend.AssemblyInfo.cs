5eaf7a4ce18b917a5e606e5ca202e9bfe7782b22
nhan bien <nhanbien1@gmail.com>
2026-05-31 21:50:57 +0700

    nộp buổi 2

--- CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
+++ CMS.Backend/obj/Debug/net8.0/CMS.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ccf13188cd5bd33e059b7da9175371efbd8c78c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c561a70cf47ce4c2f555cdae52857eb5cfe7c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMS.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
